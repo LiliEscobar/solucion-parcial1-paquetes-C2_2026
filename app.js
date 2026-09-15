5b9a4b122b8bfb2b71525834803436bfe64a91ac
00219723 <00219723@uca.edu.sv>
2026-09-15 13:20:40 -0600

    Fix: Arreglar el puerto

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const incidenciasRoutes = require('./routes/incidencias');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3124;
 
 app.use(express.json());
 app.use('/incidencias', incidenciasRoutes);
